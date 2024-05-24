f38d5c1c35e62e9f7e911fedd7294aaf2a4a2fce
Pritesh Jogdhanakar <129420569+pritesh299@users.noreply.github.com>
2024-05-25 02:37:41 +0530

    added new Chat UI

--- front_end/src/components/App.tsx
+++ front_end/src/components/App.tsx
@@ -4,25 +4,27 @@ import SearchComponent from './sidebar/searchBar'
 import ProfileHeadBar from './sidebar/userProfile'
 import ChatWindow from './chatWindow/chatWindow'
 import Status from "./status/status"
+import NewChat from "./newChat/newChat"
 
 
 function App() {
    let [viewStatus,setViewStatus]=useState(false)
+   let [viewNewContact,setViewNewContact]=useState(false)
 
   return ( 
   <>
    <div className='flex flex-row fixed w-[98vw] justify-center h-[98vh] overflow-hidden' >
-     <div className='w-[30%] min-w-[375px] h-[100%]'>
+     <div className='w-[30%] min-w-[375px] h-[100%] relative '>
       {viewStatus
-      ?
-      <Status   viewStatus={viewStatus} setViewStatus={setViewStatus}/>
-      : <>         
-         <ProfileHeadBar viewStatus={viewStatus} setViewStatus={setViewStatus} />
+      ?<Status   viewStatus={viewStatus} setViewStatus={setViewStatus}/>
+      :viewNewContact
+      ?<NewChat viewNewContact={viewNewContact} setViewNewContact={setViewNewContact}/>
+      :<>         
+         <ProfileHeadBar viewStatus={viewStatus} setViewStatus={setViewStatus} viewNewContact={viewNewContact} setViewNewContact={setViewNewContact}/>
          <SearchComponent />
          <RenderCards /> 
          </>
      }
-         
      </div>
      <div className='w-[70%]'>
         <ChatWindow />
