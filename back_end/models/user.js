4c0d25a17083b5098c2f3f2ad6344217a6d9a609
Pritesh Jogdhanakar <129420569+pritesh299@users.noreply.github.com>
2024-05-26 22:25:28 +0530

    update user.js

--- back_end/models/user.js
+++ back_end/models/user.js
@@ -4,5 +4,24 @@ import bcrypt  from "bcryptjs"
 const schema= new mongoose.Schema({
     usename:{type:String,require:true,unique:true} ,
     email:{type:String,require:true,unique:true},
-    password:{require:true} 
+    password:{require:true,type: String},
+    avatar:{require:true},
+    contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+}, { timestamps: true });
+
+UserSchema.pre('save',async ()=>{
+    if (!this.isModified('password')) {
+        return next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+     this.password=await  bcrypt.hash(this.password,salt)  
+     
+     next()
 })
+
+UserSchema.methods.MatchPasswords= async (enteredPassword)=>{
+    return bcrypt.compare(enteredPassword,this.password)
+}
+
+export default mongoose.model('User', UserSchema);
